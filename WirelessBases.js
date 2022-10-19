305260442b26fd05f07471ffa3d80f49d8b5f455
MichalQW <michalwalczakpoczta@gmail.com>
2022-10-19 21:31:42 +0200

    clearer solution

--- WirelessBases.js
+++ WirelessBases.js
@@ -12,45 +12,53 @@ class WirelessBases {
       }
 
       #keysEnum = {
-        2_1: { controller: "1", name: "LEFT", number: 1, byte: "00000001" },
-        2_2: { controller: "1", name: "RIGHT", number: 2, byte: "00000010" },
-        2_3: { controller: "1", name: "UP", number: 3, byte: "00000100" },
-        2_4: { controller: "1", name: "DOWN", number: 4, byte: "00001000" },
-        2_5: { controller: "1", name: "START", number: 5, byte: "00010000" },
-        2_6: { controller: "1", name: "SELECT", number: 6, byte: "00100000" },
+        "1": { controller: "1", name: "LEFT", number: 1, byte: "00000001" },
+        "2": { controller: "1", name: "RIGHT", number: 2, byte: "00000010" },
+        "3": { controller: "1", name: "UP", number: 3, byte: "00000100" },
+        "4": { controller: "1", name: "DOWN", number: 4, byte: "00001000" },
+        "5": { controller: "1", name: "START", number: 5, byte: "00010000" },
+        "6": { controller: "1", name: "SELECT", number: 6, byte: "00100000" },
 
-        2_7: { controller: "2", name: "LEFT", number: 7, byte: "01000000" },
-        2_8: { controller: "2", name: "RIGHT", number: 8, byte: "10000000" },
-        3_1: { controller: "2", name: "UP", number: 9, byte: "00000001" },
-        3_2: { controller: "2", name: "DOWN", number: 10, byte: "00000010" },
-        3_3: { controller: "2", name: "START", number: 11, byte: "00000100" },
-        3_4: { controller: "2", name: "SELECT", number: 12, byte: "00001000" },
+        "7": { controller: "2", name: "LEFT", number: 7, byte: "01000000" },
+        "8": { controller: "2", name: "RIGHT", number: 8, byte: "10000000" },
+        "9": { controller: "2", name: "UP", number: 9, byte: "00000001" },
+        "10": { controller: "2", name: "DOWN", number: 10, byte: "00000010" },
+        "11": { controller: "2", name: "START", number: 11, byte: "00000100" },
+        "12": { controller: "2", name: "SELECT", number: 12, byte: "00001000" },
 
-        3_5: { controller: "3", name: "LEFT", number: 13, byte: "00010000" },
-        3_6: { controller: "3", name: "RIGHT", number: 14, byte: "00100000" },
-        3_7: { controller: "3", name: "UP", number: 15, byte: "01000000" },
-        3_8: { controller: "3", name: "DOWN", number: 16, byte: "10000000" },
-        4_1: { controller: "3", name: "START", number: 17, byte: "00000001" },
-        4_2: { controller: "3", name: "SELECT", number: 18, byte: "00000010" },
+        "13": { controller: "3", name: "LEFT", number: 13, byte: "00010000" },
+        "14": { controller: "3", name: "RIGHT", number: 14, byte: "00100000" },
+        "15": { controller: "3", name: "UP", number: 15, byte: "01000000" },
+        "16": { controller: "3", name: "DOWN", number: 16, byte: "10000000" },
+        "17": { controller: "3", name: "START", number: 17, byte: "00000001" },
+        "18": { controller: "3", name: "SELECT", number: 18, byte: "00000010" },
 
-        4_3: { controller: "4", name: "LEFT", number: 19, byte: "00000100" },
-        4_4: { controller: "4", name: "RIGHT", number: 20, byte: "00001000" },
-        4_5: { controller: "4", name: "UP", number: 21, byte: "00010000" },
-        4_6: { controller: "4", name: "DOWN", number: 22, byte: "00100000" },
-        4_7: { controller: "4", name: "START", number: 23, byte: "01000000" },
-        4_8: { controller: "4", name: "SELECT", number: 24, byte: "10000000" },
+        "19": { controller: "4", name: "LEFT", number: 19, byte: "00000100" },
+        "20": { controller: "4", name: "RIGHT", number: 20, byte: "00001000" },
+        "21": { controller: "4", name: "UP", number: 21, byte: "00010000" },
+        "22": { controller: "4", name: "DOWN", number: 22, byte: "00100000" },
+        "23": { controller: "4", name: "START", number: 23, byte: "01000000" },
+        "24": { controller: "4", name: "SELECT", number: 24, byte: "10000000" },
       };
 
       process = function (data) {
-        const bytes = [...data].map(byteToBinaryString).join(" ").split(" ");
-        const reversed = reverse(bytes[1]);
+        let bytes = [...data].map(byteToBinaryString).join(" ").split(" ")
+        bytes.shift()
+        for (var i = 0; i < bytes.length; i++) {
+          bytes[i] = reverse(bytes[i]);
+        }
+        bytes = bytes.join("");
 
         const indices = [];
-        for (var i = 0; i < reversed.length; i++) {
-          if (reversed[i] === "1") indices.push(i + 1);
+        const response = [];
+        for (var i = 0; i < bytes.length; i++) {
+          if (bytes[i] === "1") indices.push(i + 1);
         }
+        indices.forEach((key) => {
+          response.push(this.#keysEnum[key])
+        });
 
-        console.log(indices);
+        console.log(response)
         return this;
       };
     };
